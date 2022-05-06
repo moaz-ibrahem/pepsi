d64a90f626daac49bca29893b90e418987770410
moaz-ibrahem <moazibrahem952@gmail.com>
2022-05-07 01:37:55 +0200

    first commit

---
+++ j.s/master.js
@@ -0,0 +1,7 @@
+function imgslider (anything) {
+    document.querySelector(".pepsi1").src = anything;
+}
+function changBgcolor(color) {
+    const sec = document.querySelector(".sec");
+    sec.style.backgroundColor = color;
+}
